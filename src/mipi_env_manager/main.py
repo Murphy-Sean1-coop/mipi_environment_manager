01aff66d0712efdb7d916f8e65040cfcf5c5fd2f
Murphy-Sean1-coop <murphy-sean1@cooperhealth.edu>
2025-03-18 11:50:45 -0400

    initial_commit

---
+++ src/mipi_env_manager/main.py
@@ -0,0 +1,417 @@
+import os
+import requests
+import yaml
+from packaging import version
+from abc import ABC, abstractmethod
+from typing import List
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from pathlib import Path
+import click
+
+ENV_GHTOKEN = "GH_TOKEN"
+ENV_SETUP_PATH = "MIPI_DEVOPS_PATH"
+
+
+def get_environ(name):
+    val = os.environ.get(name)
+    if val is None:
+        raise EnvironmentError(f'{name} is not set')
+    return val
+
+
+class Setup(ABC):
+
+    @abstractmethod
+    def _get_path(self) -> Path:
+        raise NotImplementedError  # pragma: no cover
+
+    @abstractmethod
+    def get_config(self) -> dict:
+        raise NotImplementedError  # pragma: no cover
+
+
+class YmlSetup(Setup):
+
+    def __init__(self, environ_path_name):
+        self.environ_path_name = environ_path_name
+
+    def _get_path(self) -> Path:
+        path = get_environ(self.environ_path_name)
+        return Path(path)
+
+    def get_config(self) -> dict:
+        path = self._get_path()
+        with open(path, "r") as f:
+            content = yaml.safe_load(f)
+        return content
+
+
+class Auth(ABC):
+
+    @abstractmethod
+    def get_headers(self):
+        raise NotImplementedError  # pragma: no cover
+
+    @abstractmethod
+    def get_credintials(self):
+        raise NotImplementedError  # pragma: no cover
+
+
+class GHPatAuth(Auth):
+
+    def __init__(self, environ_token_name):
+        self.environ_token_name = environ_token_name
+
+    def get_credintials(self):
+        return get_environ(self.environ_token_name)
+
+    def get_headers(self):
+        return {
+            "Authorization": f"token {self.get_credintials()}",
+            "User-Agent": "Python"
+        }
+
+
+class RepoRequest(ABC):
+
+    @property
+    @abstractmethod
+    def url(self) -> str:
+        raise NotImplementedError  # pragma: no cover
+
+
+class GHRequest(RepoRequest):
+    base_url = "https://api.github.com/repos"
+    url_suffix = "releases"
+
+    def __init__(self, user_name: str, repo_name: str, auth: Auth):
+        self.user_name = user_name
+        self.repo_name = repo_name
+        self.auth = auth
+
+    @property
+    def url(self):
+        return f"{self.base_url}/{self.user_name}/{self.repo_name}/{self.url_suffix}"
+
+    def get_repo_releases(self) -> list:
+        # Define the API endpoint for listing all releases
+
+        # Make the GET request to the GitHub API
+        response = requests.get(self.url, headers=self.auth.get_headers())
+        response.raise_for_status()  # raise an error for bad responses
+
+        # Parse the JSON response (list of releases)
+        releases = response.json()
+        return releases
+
+
+class Releases(ABC):
+
+    @abstractmethod
+    def get_latest_minor(self):
+        raise NotImplementedError  # pragma: no cover
+
+    @abstractmethod
+    def get_latest(self):
+        raise NotImplementedError  # pragma: no cover
+
+
+class GHTagReleases(Releases):
+
+    def __init__(self, releases: list, current_version):
+        self.releases = releases
+        self.current_version = current_version
+
+    def get_latest_minor(self) -> str:
+
+        current_major = version.parse(self.current_version).major
+
+        # Prepare a list to hold valid releases (of the same major version)
+        valid_versions = []
+        for release in self.releases:
+            tag = release.get("tag_name", "")
+            # Remove a leading "v" if present (common in semantic version tags)
+            tag_clean = tag.lstrip("v")
+            try:
+                ver = version.parse(tag_clean)
+                # Only consider versions with the same major version as our current version.
+                if ver.major == current_major:
+                    valid_versions.append(ver)
+            except Exception as e:
+                # Skip tags that don't parse correctly
+                continue
+
+        if not valid_versions:
+            return self.current_version
+        else:
+            # Determine the latest version from the filtered list
+            return max(valid_versions)  # TODO create max filter func
+
+    def get_latest(self):
+        # TODO check valid version
+        valid_versions = []
+        for release in self.releases:
+            tag = release.get("tag_name", "")
+            tag_clean = tag.lstrip("v")
+            try:
+                ver = version.parse(tag_clean)
+                valid_versions.append(ver)
+            except Exception:
+                continue
+
+        if not valid_versions:
+            return self.current_version
+        else:
+            return str(max(valid_versions))
+
+
+class Version(ABC):
+
+    def __init__(self, policy="latest", version_str=None):
+        self.version_str = version_str
+        if version_str is not None and policy is None:
+            self.policy = "exact"
+        else:
+            self.policy = policy
+        if self.policy == "exact" and version_str is None:
+            raise ValueError("version_str cannot be None if policy is 'exact'")
+
+    @abstractmethod
+    def format(self) -> str:
+        raise NotImplementedError  # pragma: no cover
+
+    def build(self) -> str:
+        return self.format()
+
+
+class PyPiVersion(Version):
+
+    def format(self):
+        if self.version_str is None:
+            return ""
+        else:
+            if self.policy == "exact":
+                return f"=={self.version_str}"  # Question i dont like how this is responsible for the == while the other string prefixes belong to the ReqString class
+            elif self.policy == "no_major_increment":
+                return f"~={self.version_str}"
+
+
+class GHVersion(Version):
+    def __init__(self, user, repo, policy, version_str=None):
+        super().__init__(policy, version_str)
+        self.repo = repo
+        self.user = user
+
+    def _get_releases(self):
+        auth = GHPatAuth(ENV_GHTOKEN)
+        req = GHRequest(self.user, self.repo, auth)  # TODO abstract this
+        releases = req.get_repo_releases()
+        return GHTagReleases(releases, self.version_str)
+
+    def format(self):
+        if self.version_str is None:
+            return ""
+        else:
+            if self.policy == "exact":
+                return f"v{self.version_str}"
+            elif self.policy == "no_major_increment":
+                rel_obj = self._get_releases()  # TODO Abstrac this
+                return f"v{rel_obj.get_latest_minor()}"
+
+
+class ReqString:
+
+    def __init__(self):
+        self._req_string = []
+
+    def _add_part(self, part):
+        self._req_string.append(part)
+
+    def add_name(self, name):
+        self._add_part(f"{name}")
+
+    def build(self):
+        return "".join(self._req_string)
+
+
+class PypiReqString(ReqString):
+
+    def add_version(self, policy, version_str):
+        version = PyPiVersion(policy, version_str).build()
+        self._add_part(f"{version}")
+
+
+class GHReqString(ReqString):
+
+    def add_path(self, path):
+        self._add_part(f" @ git+{path}.git")
+
+    def add_tag(self, user, repo, policy, version_str):
+        tag = GHVersion(user, repo, policy, version_str).build()
+        self._add_part(f"@{tag}")
+
+    def add_egg(self, name):
+        self._add_part(f"#egg={name}")
+
+
+class Package(ABC):
+
+    def __init__(self, req_string: ReqString, name, policy, version_str=None):
+        self.name = name
+        self.policy = policy
+        self._req_string = req_string
+        self.version_str = version_str
+
+    @abstractmethod
+    def req_string(self) -> str:
+        raise NotImplementedError  # pragma: no cover
+
+
+class PyPiPackage(Package):
+    def __init__(self, name, policy, version_str=None):
+        super().__init__(PypiReqString(), name, policy, version_str)
+
+    def req_string(self) -> str:
+        self._req_string.add_name(self.name)
+        self._req_string.add_version(self.policy,
+                                     self.version_str)  # Question warning because parent class has req_string hinted as ReqString not PypiReqString #codesmell if version is None it adds a blank string
+        return self._req_string.build()
+
+
+class GitHubPackage(Package):
+    def __init__(self, name, policy, path, version_str=None):
+        super().__init__(GHReqString(), name, policy, version_str)
+        self.path = path
+
+    def parse_path(self) -> List[str]:
+        truncated_path = self.path.removeprefix("https://github.com/")
+        return truncated_path.split("/")
+
+    def req_string(self):
+        self._req_string.add_name(self.name)
+        self._req_string.add_path(self.path)
+        user, repo = self.parse_path()
+        if self.version_str:  # TODO maybe move this condition
+            self._req_string.add_tag(user, repo, self.policy, self.version_str)
+        self._req_string.add_egg(self.name)
+
+        return self._req_string.build()
+
+
+class PkgFactory(ABC):
+
+    @abstractmethod
+    def create(self, name, vals):
+        raise NotImplementedError  # pragma: no cover
+
+
+class Pypi(PkgFactory):
+    def create(self, name, vals):
+        return PyPiPackage(name, vals.get("version_policy"), vals.get("version"))
+
+
+class Gh(PkgFactory):
+    def create(self, name, vals):
+        return GitHubPackage(name, vals.get("version_policy"), vals.get("path"), vals.get("version"))
+
+
+class Dependancies():
+    def __init__(self, config):
+        self.config = config
+        self.dict_ = {
+            "github": Gh,
+            "pypi": Pypi
+        }
+
+    def _read_dependencies(self):
+        return self.config["packages"]
+
+    def create_strings(self):
+        dependencies = []
+        for k, v in self._read_dependencies().items():
+            pkg = self.dict_[v["source"]]()
+            dependencies.append(pkg.create(k, v).req_string())
+        return "\n".join(dependencies)
+
+
+class BatInstaller:
+
+    def __init__(self, template, out_path):
+        self.template = template
+        self.out_path = out_path
+
+    def _get_out_path(self, name, subdir):
+        if subdir:
+            _path = os.path.join(self.out_path, subdir, name)
+        else:
+            _path = os.path.join(self.out_path, name)
+        return _path
+
+    def _get_template(self):
+        env = Environment(loader=FileSystemLoader("templates"), autoescape=select_autoescape())
+        return env.get_template(self.template)
+
+    def _render_template(self, **kwargs):
+        temp = self._get_template()
+        content = temp.render(**kwargs)
+        return content
+
+    def _maybe_create_subdir(self, subdir):
+        if subdir:
+            _path = os.path.join(self.out_path, subdir)
+        else:
+            _path = self.out_path
+        if not os.path.exists(_path):
+            os.makedirs(_path)
+
+    def create(self, name, subdir=None, **kwargs):
+        content = self._render_template(**kwargs)
+        file_path = self._get_out_path(name, subdir)
+        self._maybe_create_subdir(subdir)
+        with open(file_path, "w") as f:
+            f.write(content)
+
+
+class PublishInstallers:
+    def __init__(self, setup: Setup):
+        self.setup = setup
+        self.config = self.get_config() # TODO i dont like having function calls in the init
+
+    def get_config(self):
+        return self.setup.get_config()
+
+    def publish(self):
+        envs = self.config["environments"]
+        outpath = self.config["setup"]["outpath"]
+
+        envs_to_include_in_master_installer = []
+
+        for env, config in envs.items():
+            bat = BatInstaller("env_installer.bat.jinja", outpath)
+            bat.create("create_env.bat", subdir=env, env_name=env, create_env=True,
+                       py_version=config["setup"]["py_version"])
+            bat.create("update_env.bat", subdir=env, env_name=env, create_env=False,
+                       py_version=config["setup"]["py_version"])
+
+            if config["setup"]["include_in_master"]:
+                envs_to_include_in_master_installer.append(os.path.join(outpath, env))
+
+        mbat = BatInstaller("master_installer.bat.jinja", outpath)
+        mbat.create("master_update_env.bat",
+                    environment_variables=self.config["setup"]["environment_variables"],
+                    installers=envs_to_include_in_master_installer)
+
+        for env, config in envs.items():
+            deps = Dependancies(config)
+            with open(os.path.join(outpath, env, "requirements.txt"), "w") as f:
+                f.write(deps.create_strings())
+
+@click.command()
+def main():
+    setup = YmlSetup(ENV_SETUP_PATH)
+    publisher = PublishInstallers(setup)
+    publisher.publish()
+
+
+if __name__ == "__main__":
+    main()
---
+++ test/_test_main.py
@@ -0,0 +1,219 @@
+import pytest
+from unittest.mock import patch, MagicMock
+from tempfile import tempdir
+import os
+from jinja2 import Template
+from mipi_env_manager.main import (
+    get_env,
+    read_yml,
+    parse_path,
+    get_repo_releases,
+    get_latest_minor,
+    Version,
+    PyPiVersion,
+    GHVersion,
+    ReqString,
+    PypiReqString,
+    GHReqString,
+    Package,
+    PyPiPackage,
+    GitHubPackage,
+    PkgFactory,
+    Pypi,
+    Gh,
+    Dependancies,
+    BatInstaller,
+    create_installers,
+    main
+)
+
+
+def test_get_token(monkeypatch):
+    monkeypatch.setenv("GH_TOKEN", "token_val")
+    assert get_env("GH_TOKEN") == "token_val"
+
+
+def test_get_token_raises(monkeypatch):
+    monkeypatch.delenv("GH_TOKEN")
+    with pytest.raises(EnvironmentError):
+        print(get_env("GH_TOKEN"))
+
+
+def test_read_yml():
+    res = read_yml("test_dependencies.yml")
+    assert isinstance(res, dict)
+
+
+def test_parse_path():
+    path = "https://github.com/psf/requests"
+    assert parse_path(path) == ["psf", "requests"]
+
+
+class TestVersion:
+
+    @pytest.mark.parametrize(
+        "v,policy,res",
+        [
+            pytest.param("1.0.0", "exact", "==1.0.0", id="exact"),
+            pytest.param("1.0.0", "no_major_increment", "~=1.0.0", id="no_major_increment"),
+            pytest.param(None, None, "", id="nothin_specified"),
+            pytest.param("1.0.0", None, "==1.0.0", id="version_implies_exact"),
+        ]
+    )
+    def test_pypi_version(self, v, policy, res):
+        assert PyPiVersion(policy, version_str=v).build() == res
+
+    @pytest.mark.parametrize(
+        "v,policy,res",
+        [
+            pytest.param("1.0.0", "exact", "v1.0.0", id="exact"),
+            pytest.param("1.0.0", "no_major_increment", "v1.1.0", id="no_major_increment"),
+            pytest.param(None, None, "", id="nothin_specified"),
+            pytest.param("1.0.0", None, "v1.0.0", id="version_implies_exact"),
+        ]
+    )
+    @patch("mipi_env_manager.main.get_latest_minor")
+    def test_gh_version(self, mock_get_latest_minor, v, policy, res):
+        mock_get_latest_minor.return_value = "1.1.0"
+        path = "https://github.com/psf/requests"
+        assert GHVersion(path, policy, version_str=v).build() == res
+
+    def test_pypi_version_raises(self):
+        with pytest.raises(ValueError):
+            PyPiVersion("exact", version_str=None).build()
+
+    @patch("mipi_env_manager.main.get_latest_minor")
+    def test_gh_version_raises(self, mock_get_latest_minor):
+        mock_get_latest_minor.return_value = "1.1.0"
+        path = "https://github.com/psf/requests"
+        with pytest.raises(ValueError):
+            GHVersion(path, "exact", version_str=None).build()
+
+
+class TestReqString:
+
+    def test_pypi_reqstring(self):
+        obj = PypiReqString()
+        assert obj.build() == ""
+
+        obj.add_name("requests")
+        assert obj.build() == "requests"
+
+        obj.add_version("exact", "1.0.0")  # TODO change these to 1.1.0 for consistancey
+        assert obj.build() == "requests==1.0.0"
+
+    @patch("mipi_env_manager.main.get_latest_minor")
+    def test_gh_reqstring(self, mock_get_latest_minor):
+        obj = GHReqString()
+        assert obj.build() == ""
+
+        obj.add_name("requests")
+        assert obj.build() == "requests"
+
+        obj.add_path("https://github.com/psf/requests")
+        assert obj.build() == "requests @ git+https://github.com/psf/requests.git"
+
+        mock_get_latest_minor.return_value = "1.1.0"
+        obj.add_tag("https://github.com/psf/requests", "exact", "1.1.0")
+        assert obj.build() == "requests @ git+https://github.com/psf/requests.git@v1.1.0"
+
+        obj.add_egg("requests")
+        assert obj.build() == "requests @ git+https://github.com/psf/requests.git@v1.1.0#egg=requests"
+
+
+class TestPackage:
+
+    def test_pypi_package(self):
+        assert PyPiPackage("mypackage", "exact", version_str="1.0.0").req_string() == "mypackage==1.0.0"
+        assert PyPiPackage("mypackage", "no_major_increment", version_str="1.0.0").req_string() == "mypackage~=1.0.0"
+
+    @patch("mipi_env_manager.main.get_latest_minor")
+    def test_gh_package(self, mock_get_latest_minor):
+        mock_get_latest_minor.return_value = "1.1.0"
+        # todo make paths consistant with pkgname
+        assert GitHubPackage("mypackage", "exact", path="https://github.com/psf/requests",
+                             version_str="1.0.0").req_string() == "mypackage @ git+https://github.com/psf/requests.git@v1.0.0#egg=mypackage"
+        assert GitHubPackage("mypackage", "no_major_increment", path="https://github.com/psf/requests",
+                             version_str="1.0.0").req_string() == "mypackage @ git+https://github.com/psf/requests.git@v1.1.0#egg=mypackage"
+
+
+class TestFactory:
+
+    def test_pypi_factory(self):
+        factory = Pypi()
+        assert factory.create("mypackage", {"source": "pypi"}).req_string() == "mypackage"
+        assert factory.create("mypackage", {"source": "pypi", "version": "1.0.0"}).req_string() == "mypackage==1.0.0"
+        assert factory.create("mypackage", {"source": "pypi", "version": "1.0.0",
+                                            "version_policy": "exact"}).req_string() == "mypackage==1.0.0"
+        assert factory.create("mypackage", {"source": "pypi", "version": "1.0.0",
+                                            "version_policy": "no_major_increment"}).req_string() == "mypackage~=1.0.0"
+
+    @patch("mipi_env_manager.main.get_latest_minor")
+    def test_gh_factory(self, mock_get_latest_minor):
+        factory = Gh()
+        mock_get_latest_minor.return_value = "1.1.0"
+        assert factory.create("mypackage", {"source": "pypi",
+                                            "path": "https://github.com/psf/requests"}).req_string() == "mypackage @ git+https://github.com/psf/requests.git#egg=mypackage"
+        assert factory.create("mypackage", {"source": "pypi", "version": "1.0.0",
+                                            "path": "https://github.com/psf/requests"}).req_string() == "mypackage @ git+https://github.com/psf/requests.git@v1.0.0#egg=mypackage"  # TODO defaults to exact version, do i want this for GH?
+        assert factory.create("mypackage", {"source": "pypi", "version": "1.0.0", "version_policy": "exact",
+                                            "path": "https://github.com/psf/requests"}).req_string() == "mypackage @ git+https://github.com/psf/requests.git@v1.0.0#egg=mypackage"
+        assert factory.create("mypackage",
+                              {"source": "pypi", "version": "1.0.0", "version_policy": "no_major_increment",
+                               "path": "https://github.com/psf/requests"}).req_string() == "mypackage @ git+https://github.com/psf/requests.git@v1.1.0#egg=mypackage"
+
+
+@patch("mipi_env_manager.main.get_latest_minor")
+def test_dependancies(mock_get_latest_minor):
+    mock_get_latest_minor.return_value = "1.1.0"
+    config = read_yml("test_dependencies.yml")["environments"]["myenv"]
+    res = Dependancies(config).create_strings()
+
+    with open("expected_reqs.txt", "r") as f:
+        expected_reqs = f.read()
+
+    assert res == expected_reqs
+
+
+
+class TestBatInstaller:
+    @pytest.fixture(scope="class")
+    def shared_temp_dir(self, tmp_path_factory):
+        return tmp_path_factory.mktemp("shared_temp_dir")
+
+    @pytest.fixture(scope="class")
+    def installer(self, shared_temp_dir):
+        return BatInstaller("test_bat.jinja", shared_temp_dir)
+
+    def test_construct(self, installer, shared_temp_dir):
+        assert isinstance(installer, BatInstaller)
+        assert installer.out_path == shared_temp_dir
+        assert installer.template == "test_bat.jinja"
+
+    def test_get_outpath(self, installer, shared_temp_dir):
+        assert installer._get_out_path("file.bat", "env1") == os.path.join(shared_temp_dir, "env1", "file.bat")
+        assert installer._get_out_path("file.bat", None) == os.path.join(shared_temp_dir, "file.bat")
+
+    def test_get_template(self, installer):
+        assert isinstance(installer._get_template(), Template)
+
+    def test_render_template(self, installer):
+        str_ = installer._render_template(val1="val1", val2="val2")
+        assert str_ == "val1 val2"
+
+    def test_create(self, installer, shared_temp_dir):
+        installer.create("create.bat", "env1", val1="val1", val2="val2")
+
+        dir_ = os.path.join(shared_temp_dir, "env1")
+        assert os.listdir(dir_) == ["create.bat"]
+
+        with open(os.path.join(dir_, "create.bat"), "r") as f:
+            content = f.read()
+
+        assert content == "val1 val2"
+
+
+
+
+
+
